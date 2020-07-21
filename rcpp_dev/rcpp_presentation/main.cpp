214692845d43e721173486dff3b05beb2e99a511
Sebastian Hanß <sebastian@hanss.info>
2020-07-21 17:18:53 +0200

    Added RInside

--- rcpp_dev/rcpp_presentation/main.cpp
+++ rcpp_dev/rcpp_presentation/main.cpp
@@ -1,9 +1,17 @@
-#include <iostream>
+#include "RInside.h"
+#include "../../src/mat_mult_naive_rcpp.h"
 
-using namespace std;
+using namespace Rcpp;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    RInside R; // opens an R session
+
+    R.parseEvalQ("size <- 20");
+    NumericMatrix a = R.parseEval("matrix(runif(size * size), ncol = size, nrow = size)");
+    NumericMatrix b = R.parseEval("matrix(runif(size * size), ncol = size, nrow = size)");
+
+    auto result = mat_mult_naive_rcpp(a, b);
+
     return 0;
 }
